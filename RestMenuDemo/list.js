b7f7be8f342997a9f5873202212a131dac6340af
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-14 20:24:38 +0530

    adding list item end point

---
+++ RestMenuDemo/list.js
@@ -0,0 +1,32 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+
+	let response = {
+		"statusCode": 200,
+		"headers": {
+			"headerName": "headerValue"
+		},
+		"body": "..."
+	};
+	let itemType = (event.queryStringParameters && event.queryStringParameters.type) || "NON_VEG";
+
+	ddb.scan({
+		TableName: 'MenuItems',
+		ExpressionAttributeValues: {
+			':it': itemType
+		},
+		FilterExpression: 'itemType = :it'
+	}, function (err, data) {
+		if(!err && data.Items){
+			response.body = JSON.stringify(data.Items);
+		}else{
+			response.body = "No Items";
+		}
+		callback(err, response);
+	});
+
+
+
+
+}
