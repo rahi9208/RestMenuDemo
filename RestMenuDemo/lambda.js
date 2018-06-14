268df5121d35d607f5a770b85e3e054088aa7135
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-14 20:15:19 +0530

    adding dynamodb

--- RestMenuDemo/lambda.js
+++ RestMenuDemo/lambda.js
@@ -1,6 +1,13 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
 
-console.log("Hellow Bright Talk")
-	callback(null,'Successfully executed');
+	ddb.put({
+		TableName: 'MenuItems',
+		Item: { 'itemCode': event.itemCode, 'itemName': event.itemName, 'itemPrice': event.itemPrice, 'itemType': event.itemType }
+	}, function (err, data) {
+		callback(err, 'Successfully persisted');
+	});
+
+	
 }
